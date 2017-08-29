59747488885d6aea2d9f508f820a1653fa9070c4
Abid M. Khan <amkhan@CS.NET>
2017-08-29 06:05:47 +0100

    Add project files.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonas Group")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Jonas Group 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63bb1955-46e0-47dc-9571-46753a04853a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod, TestCategory("Smoke Test")]
+        public void TestCase1_LoginWithValidCrendentials()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("www.adactin.com/HotelApp");
+
+            IWebElement my_userName = driver.FindElement(By.Id("username"));
+            my_userName.SendKeys("abidtester");
+
+            IWebElement my_password = driver.FindElement(By.Id("password"));
+            my_password.SendKeys("tester");
+
+            IWebElement submit_Btn = driver.FindElement(By.Id("login"));
+            submit_Btn.Click();
+
+            Thread.Sleep(5000);
+
+            string welcomeMessage = driver.FindElement(By.Id("username_show")).GetAttribute("value");
+
+            Assert.AreEqual(welcomeMessage, "Hello abidtester!");
+
+            driver.Quit();
+        }
+
+        [TestMethod, TestCategory("Regression Test")]
+        public void TestCase2_LoginWithInvalidCrendentials()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("www.adactin.com/HotelApp");
+
+            IWebElement my_userName = driver.FindElement(By.Id("username"));
+            my_userName.SendKeys("abidtester");
+
+            IWebElement my_password = driver.FindElement(By.Id("password"));
+            my_password.SendKeys("tester");
+
+            IWebElement submit_Btn = driver.FindElement(By.Id("login"));
+            submit_Btn.Click();
+
+            Thread.Sleep(5000);
+
+            string welcomeMessage = driver.FindElement(By.Id("username_show")).GetAttribute("value");
+
+            Assert.AreEqual(welcomeMessage, "Hello abidtester!");
+
+            driver.Quit();
+        }
+
+    }
+}
